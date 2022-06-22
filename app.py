22e25d7dd9085a43707f809c47d567474a31b5b0
liniiker <elimar.fabio1@gmail.com>
2022-06-22 17:18:52 -0300

    second

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
 
 header_list = ['Time', 'Estado de Carga', 'Velocidade', 'Temperatura1', 'Temperatura2', 'Temperatura3', 'Temperatura4', 'RPM1', 'RPM2', 'RPM3', 'RPM4', 'TEMPERATURA MOTOR']
 arquivo = pd.read_csv('bitcoin_data.csv', names=header_list)
-t1_r1 = arquivo['Temperatura1'].tail(1).iloc[0]
+
 
 card_style = {
     'padding':'10px'
@@ -51,47 +51,86 @@ app.layout = html.Div([
     dbc.Row([
         dbc.Col([
             dbc.Card([
-                dbc.CardHeader(html.H1('este é o1 titulo')),
-                dbc.CardBody('este é o corpo')
+                dbc.CardHeader(html.H1('card 1')),
+                dbc.CardBody(html.Div(id='text_row1'))
             ], color='success', style = card_style)
         ], width= 4),
         dbc.Col([
             dbc.Card([
-                dbc.CardHeader(html.H1('este é o titulo')),
-                dbc.CardBody('este é o corpo')
+                dbc.CardHeader(html.H1('card 2')),
+                dbc.CardBody(html.Div(id='text_row2'))
             ], color='success', style = card_style)
         ], width= 4),
         dbc.Col([
             dbc.Card([
-                dbc.CardHeader(html.H1('este é o titulo')),
-                dbc.CardBody('este é o corpo')
-            ], color='success', style = card_style)
-        ], width = 4)
-    ], className="mb-4"),
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader(html.H1('este é o1 titulo')),
-                dbc.CardBody('este é o corpo')
+                dbc.CardHeader(html.H1('card 3')),
+                dbc.CardBody(html.Div(id='text_row3'))
             ], color='success', style = card_style)
         ], width= 4),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader(html.H1('este é o titulo')),
-                dbc.CardBody('este é o corpo')
-            ], color='success', style = card_style)
-        ], width= 4),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader(html.H1('este é o titulo')),
-                dbc.CardBody('este é o corpo')
-            ], color='success', style = card_style)
-        ], width = 4)
     ], className="mb-4"),
 ])
 
+@app.callback(Output('text_row1','children'),
+                [Input('update_value','n_intervals')])
+def update_card(n_intervals):
+    if n_intervals == 0:
+        raise PreventUpdate
+    else:
+        t1_r1 = arquivo['Temperatura1'].tail(1).iloc[0]
+    if t1_r1 > 60:
+        return [
+                    html.Div([
+                        html.H6('{0:,.2f}'.format(t1_r1),
+                                style={'color': 'red',
+                                       'font-weight': 'bold'},
+                                className='coin_price'),
+                        html.H6('{0:,.2f}'.format(t1_r1),
+                                style={'color': 'red',
+                                       'font-weight': 'bold'},),
+                    ]),
+                
+        ]
 
+@app.callback(Output('text_row2','children'),
+                [Input('update_value','n_intervals')])
+def update_card(n_intervals):
+    if n_intervals == 0:
+        raise PreventUpdate
+    else:
+        t2_r1 = arquivo['Temperatura2'].tail(1).iloc[0]
+    if t2_r1 > 60:
+        return [
+                    html.Div([
+                        html.H6('{0:,.2f}'.format(t2_r1),
+                                style={'color': 'red',
+                                       'font-weight': 'bold'},
+                                className='coin_price'),
+                        html.H6('{0:,.2f}'.format(t2_r1),
+                                style={'color': 'red',
+                                       'font-weight': 'bold'},),
+                    ]),
+                
+        ]
 
-
+@app.callback(Output('text_row3','children'),
+                [Input('update_value','n_intervals')])
+def update_card(n_intervals):
+    if n_intervals == 0:
+        raise PreventUpdate
+    else:
+        t3_r1 = arquivo['Temperatura3'].tail(1).iloc[0]
+    if t3_r1 > 60:
+        return [
+                    html.Div([
+                        html.H6('{0:,.2f}'.format(t3_r1),
+                                style={'color': 'red',
+                                       'font-weight': 'bold'},
+                                className='coin_price'),
+                        html.H6('{0:,.2f}'.format(t3_r1),
+                                style={'color': 'red',
+                                       'font-weight': 'bold'},),
+                    ]),
+                
+        ]
 if __name__ == '__main__':
     app.run_server(debug = True, port = 2020)
